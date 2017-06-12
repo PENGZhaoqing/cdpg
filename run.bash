538f89b5bb98446aa158cb29b4b018ec13986c67
peng <ppp8300885@gmail.com>
2017-06-13 00:48:09 +0800

    change bash

--- run.bash
+++ run.bash
@@ -19,28 +19,28 @@
 
 
 # --basic signal 1--
-#/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 32 --signal-num 1 --a1-AC-folder basic_signal_1/a1_AC --a2-AC-folder basic_signal_1/a2_AC  --a1-CDPG-folder basic_signal_1/a1_CDPG  --a2-CDPG-folder basic_signal_1/a2_CDPG --save-log  basic_signal_1/log
+/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 32 --signal-num 1 --a1-AC-folder basic_signal_1/a1_AC --a2-AC-folder basic_signal_1/a2_AC  --a1-CDPG-folder basic_signal_1/a1_CDPG  --a2-CDPG-folder basic_signal_1/a2_CDPG --save-log  basic_signal_1/log
 
 # --basic signal 2--
-#/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 2 --a1-AC-folder basic_signal_2/a1_AC --a2-AC-folder basic_signal_2/a2_AC  --a1-CDPG-folder basic_signal_2/a1_CDPG  --a2-CDPG-folder basic_signal_2/a2_CDPG --save-log  basic_signal_2/log
+/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 2 --a1-AC-folder basic_signal_2/a1_AC --a2-AC-folder basic_signal_2/a2_AC  --a1-CDPG-folder basic_signal_2/a1_CDPG  --a2-CDPG-folder basic_signal_2/a2_CDPG --save-log  basic_signal_2/log
 
 # --basic signal 3--
-# /usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 34 --continue-training 0 --start-epoch 40 --signal-num 3 --a1-AC-folder basic_signal_3/a1_AC --a2-AC-folder basic_signal_3/a2_AC  --a1-CDPG-folder basic_signal_3/a1_CDPG  --a2-CDPG-folder basic_signal_3/a2_CDPG --save-log  basic_signal_3/log
+/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 34 --continue-training 0 --start-epoch 40 --signal-num 3 --a1-AC-folder basic_signal_3/a1_AC --a2-AC-folder basic_signal_3/a2_AC  --a1-CDPG-folder basic_signal_3/a1_CDPG  --a2-CDPG-folder basic_signal_3/a2_CDPG --save-log  basic_signal_3/log
 
 # --basic signal 4--
-#/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 4 --a1-AC-folder basic_signal_4/a1_AC --a2-AC-folder basic_signal_4/a2_AC  --a1-CDPG-folder basic_signal_4/a1_CDPG  --a2-CDPG-folder basic_signal_4/a2_CDPG --save-log  basic_signal_4/log
+/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 4 --a1-AC-folder basic_signal_4/a1_AC --a2-AC-folder basic_signal_4/a2_AC  --a1-CDPG-folder basic_signal_4/a1_CDPG  --a2-CDPG-folder basic_signal_4/a2_CDPG --save-log  basic_signal_4/log
 
 # --basic signal 6--
-#/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 6 --a1-AC-folder basic_signal_6/a1_AC --a2-AC-folder basic_signal_6/a2_AC  --a1-CDPG-folder basic_signal_6/a1_CDPG  --a2-CDPG-folder basic_signal_6/a2_CDPG --save-log  basic_signal_6/log
+/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 6 --a1-AC-folder basic_signal_6/a1_AC --a2-AC-folder basic_signal_6/a2_AC  --a1-CDPG-folder basic_signal_6/a1_CDPG  --a2-CDPG-folder basic_signal_6/a2_CDPG --save-log  basic_signal_6/log
 
 # --basic signal 8--
-# /usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 8 --a1-AC-folder basic_signal_8/a1_AC --a2-AC-folder basic_signal_8/a2_AC  --a1-CDPG-folder basic_signal_8/a1_CDPG  --a2-CDPG-folder basic_signal_8/a2_CDPG --save-log  basic_signal_8/log
+/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 40 --signal-num 8 --a1-AC-folder basic_signal_8/a1_AC --a2-AC-folder basic_signal_8/a2_AC  --a1-CDPG-folder basic_signal_8/a1_CDPG  --a2-CDPG-folder basic_signal_8/a2_CDPG --save-log  basic_signal_8/log
 
 # --basic signal 16--
 # /usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 32 --signal-num 16 --a1-AC-folder basic_signal_16/a1_AC --a2-AC-folder basic_signal_16/a2_AC  --a1-CDPG-folder basic_signal_16/a1_CDPG  --a2-CDPG-folder basic_signal_16/a2_CDPG --save-log  basic_signal_16/log
 
 # --toxin--
-# /usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40 --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 20 --signal-num 4 --a1-AC-folder toxin/a1_AC --a2-AC-folder toxin/a2_AC  --a1-CDPG-folder toxin/a1_CDPG  --a2-CDPG-folder toxin/a2_CDPG --save-log  toxin/log
+/usr/bin/python2.7 2-agent/com/cdpg.py  --epoch-num 40 --toxin 5 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 20 --signal-num 4 --a1-AC-folder toxin/a1_AC --a2-AC-folder toxin/a2_AC  --a1-CDPG-folder toxin/a1_CDPG  --a2-CDPG-folder toxin/a2_CDPG --save-log  toxin/log
 
 
 # ---central---
@@ -52,10 +52,10 @@
 
 # ---uncom---
 # --basic--
-# /usr/bin/python2.7 2-agent/uncom/cdpg.py --t-max 1 --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 20 --signal-num 4 --a1-AC-folder basic/a1_AC --a2-AC-folder basic/a2_AC  --save-log  basic/log
+/usr/bin/python2.7 2-agent/uncom/cdpg.py --t-max 1 --epoch-num 40  --toxin 0 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 20 --signal-num 4 --a1-AC-folder basic/a1_AC --a2-AC-folder basic/a2_AC  --save-log  basic/log
 
 # --toxin--
-# /usr/bin/python2.7 2-agent/uncom/cdpg.py  --t-max 1 --epoch-num 40  --toxin 5 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 20 --signal-num 4 --a1-AC-folder toxin/a1_AC --a2-AC-folder toxin/a2_AC  --save-log  toxin/log
+/usr/bin/python2.7 2-agent/uncom/cdpg.py  --t-max 1 --epoch-num 40  --toxin 5 --testing 0 --testing-epoch 39 --continue-training 0 --start-epoch 20 --signal-num 4 --a1-AC-folder toxin/a1_AC --a2-AC-folder toxin/a2_AC  --save-log  toxin/log
 
 
 # ----3 agent----
